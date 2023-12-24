fa8aa6329d2d8472edc706c557558b24ae55f742
AshutoshGarg01 <gashu3609@gmail.com>
2023-12-24 11:07:50 +0530

    Social Media initial files - Stage 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Feed from './components/Feed'
+import Login from './components/Login'
+import SingUp from './components/SingUp'
+
+function App() {
+
+  return (
+    <>
+      <Login />
+      <SingUp />
+      <Feed />
+    </>
+  )
+}
+
+export default App
