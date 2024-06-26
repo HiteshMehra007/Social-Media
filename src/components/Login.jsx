47df492c79b5613bc1283171ab70e8de8f76e82d
HiteshMehra007 <hiteshmehra809@gmail.com>
2024-06-26 23:43:19 +0530

    updated logi and signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,46 @@
 import { ArrowRight } from 'lucide-react'
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  }
+
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in</h2>
             <p className="mt-2 text-sm text-gray-600">
               Don&apos;t have an account?{' '}
-              <a
-                href="#"
+              <Link
+                to="/signup"
                 title=""
                 className="font-semibold text-black transition-all duration-200 hover:underline"
               >
                 Create a free account
-              </a>
+              </Link>
             </p>
-            <form action="#" method="POST" className="mt-8">
+            <form onSubmit={handleSubmit} className="mt-8">
               <div className="space-y-5">
                 <div>
                   <label htmlFor="" className="text-base font-medium text-gray-900">
                     {' '}
-                    Email address{' '}
+                    Username{' '}
                   </label>
                   <div className="mt-2">
                     <input
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="email"
-                      placeholder="Email"
+                      type="text"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
                     ></input>
                   </div>
                 </div>
@@ -38,25 +50,20 @@ const Login = () => {
                       {' '}
                       Password{' '}
                     </label>
-                    <a
-                      href="#"
-                      title=""
-                      className="text-sm font-semibold text-black hover:underline"
-                    >
-                      {' '}
-                      Forgot password?{' '}
-                    </a>
                   </div>
                   <div className="mt-2">
                     <input
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                       type="password"
                       placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                     ></input>
                   </div>
                 </div>
                 <div>
                   <button
+                    onClick={handleSubmit}
                     type="button"
                     className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                   >
@@ -101,14 +108,6 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className="h-full w-full">
-          <img
-            className="mx-auto h-full w-full rounded-md object-cover"
-            src="https://images.unsplash.com/photo-1630673245362-f69d2b93880e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt=""
-          />
-        </div>
-      </div>
     </section>
   )
 }
